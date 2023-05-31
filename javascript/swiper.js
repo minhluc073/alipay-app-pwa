4c66c5b820345f2d981d61d2ca89a9147f04ba80
minhluc073 <nguyenminhluc0703@gmail.com>
2023-05-31 09:45:19 +0700

    update

--- javascript/swiper.js
+++ javascript/swiper.js
@@ -22,7 +22,6 @@ if ($("div").hasClass("swiper-container")) {
   var swiper = new Swiper(".banner-tes", {
     loop: true,
     slidesPerView: 1,
-    spaceBetween: -35,
     navigation: {
       clickable: true,
       nextEl: ".button-lo-next",
@@ -33,12 +32,34 @@ if ($("div").hasClass("swiper-container")) {
       clickable: true,
     },
     breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween:  0,
+      },
+      375: {
+        slidesPerView: 1,
+        spaceBetween:  -30,
+      },
       500: {
+        slidesPerView: 1,
+        spaceBetween:  -150,
+
+      },
+      600: {
         slidesPerView: 2,
+        spaceBetween:  30,
+
+      },
+      900: {
+        slidesPerView: 3,
+        spaceBetween:  12,
+
       },
 
       1200: {
         slidesPerView: 4,
+        spaceBetween: 12,
+
       },
     },
   });
